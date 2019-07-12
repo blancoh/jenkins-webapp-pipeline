53822b3f0360041db5ba04b41a9064a4f2ab4fcd
vagrant <vagrant@localhost.localdomain>
2019-07-11 20:44:44 -0400

    Updated Jenkinsfile

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "public_key_path" {
-   default = "/home/vagrant/.ssh/id_rsa.pub"
+  # default = "/home/vagrant/.ssh/id_rsa.pub"
+   default = "/apps/terraform/keys/id_rsa.pub"
 }
 variable "key_name" {
   description = "Desired name of AWS key pair"
