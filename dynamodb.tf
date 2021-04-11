2f0ba15671d1cecc7d8f0439e4ed8e8e929aae7e
blancoh <blancoh@users.noreply.github.com>
2021-04-10 21:58:32 -0400

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,18 +1,4 @@
-    resource "aws_s3_bucket" "terraform_state_s3_bucket" {
-        bucket = "terraform-state-file-storage"
-     
-        versioning {
-          enabled = true
-        }
-     
-        lifecycle {
-          prevent_destroy = true
-        }
-     
-        tags {
-          Name = "Terraform State File Storage"
-        }      
-    }
+
 
     resource "aws_dynamodb_table" "terraform_state_locking_dynamodb" {
       name = "terraform-state-locking"
