8842317ade198e8142113146bc63b4de12c5bc41
blancoh <blancoh@users.noreply.github.com>
2021-04-10 21:08:34 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "mybucket0000001"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    #encrypt        = true
+    encrypt        = true
     dynamodb_table = "dynamo"
   }
 }
