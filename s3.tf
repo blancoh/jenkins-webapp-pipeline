348a46052747b0c3eba30218a3a89478709f6b30
blancoh <blancoh@users.noreply.github.com>
2021-04-10 20:47:00 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,12 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-bucket-hab"
-#    key    = "terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "mybucket0000001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
 #resource "aws_s3_bucket" "terraform-bucket-alex" {
 #  bucket = "terraform-bucket-alex"
 #  acl = private
