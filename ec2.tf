9a5d85e1a2e78c0d17444e5b8629d8a31097687c
blancoh <blancoh@users.noreply.github.com>
2021-04-11 14:14:45 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -20,7 +20,7 @@ resource "aws_launch_configuration" "autoscale_web" {
               echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
               sudo apt-get -y update
               sudo apt-get -y install docker-ce docker-ce-cli containerd.io              
-              docker run -it --rm -d -p 8080:80 --name web nginx
+              docker run -it --rm -d -p 80:80 --name web nginx
               #sudo apt-get -y install nginx
               #sudo systemctl enable nginx
 EOF
