c97f641417ef47c0af34466125e9b61ca227d8e5
blancoh <blancoh@users.noreply.github.com>
2021-04-12 12:35:13 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket00000002"
+    bucket = "mybucket000000022"
     key    = "terraform-webapp.tfstate"
     region = "us-east-1"
     encrypt        = true
