8841c827b12cafcb5fadbd820863b644f0f232f9
blancoh <blancoh@users.noreply.github.com>
2021-04-12 19:57:31 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jenkins-webapp-pipeline"
-    key    = "terraform-webapp.tfstate"
+    key    = "terraform-aws/terraform-webapp.tfstate"
     region = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
