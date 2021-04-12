f8a8692098a2bf2c8498cc9220a7d05ba4e5a87b
blancoh <blancoh@users.noreply.github.com>
2021-04-12 12:17:50 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mybucket00000002"
-    key    = "terraform.tfstate"
+    key    = "terraform-webapp.tfstate"
     region = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
