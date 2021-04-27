46b40f7d54ae4bde45ee3e0f9275eda40b4f5292
blancoh <blancoh@users.noreply.github.com>
2021-04-27 14:18:51 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,8 @@ resource "aws_key_pair" "auth" {
 
 resource "aws_launch_configuration" "autoscale_web" {
   name_prefix     = "webserver-"
-  image_id        = var.aws_amis[var.aws_region]
+  #image_id        = var.aws_amis[var.aws_region]
+  image_id       = "ami-02aedf246b2c43e91"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.sec_web.id]
   key_name        = aws_key_pair.auth.id
