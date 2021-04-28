a5862084640a64d7c3fdaeb8e302bc4a39bb4393
blancoh <blancoh@users.noreply.github.com>
2021-04-28 11:13:39 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,14 +12,7 @@ resource "aws_launch_configuration" "autoscale_web" {
 
 #  associate_public_ip_address = true
   user_data = <<-EOF
-    #!/bin/bash
-    #sudo apt-get -y update
-    #sudo apt-get -y remove docker docker-engine docker.io containerd runc
-    #sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
-    #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    #echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    #sudo apt-get -y update
-    #sudo apt-get -y install docker-ce docker-ce-cli containerd.io              
+    #!/bin/bash             
     docker run -it --rm -d -p 80:80 --name web nginx
 EOF
 
